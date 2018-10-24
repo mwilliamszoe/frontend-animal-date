184b23d0072c1b192473f0bbf6b1a9256875c4a8
github username <github email address>
2018-10-24 16:36:32 -0500

    no changes

--- index.js
+++ index.js
@@ -156,9 +156,9 @@ function addUserForm() {
   document.getElementById("userform").innerHTML += `
   <form id="new-user">
     <label id="new-user-label">New User</label>
-    <input type="text" class="form-control" id="new-user" placeholder="Username">
-<button data-name="usersubmit">Submit</button>
-</form>`;
+    <input type="text" class="form-control" id="new-user" placeholder="username">
+    <button data-name="usersubmit">Submit</button>
+  </form>`;
 }
 
 function getData() {
